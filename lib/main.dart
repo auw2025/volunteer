3c1a64ba4f773eb9b4232462a591d52f9e79ddf3
Arjun Ingole <arjuningole38@gmail.com>
2023-03-04 14:32:42 +0530

    root: initialize placeholder flutter project with firebase
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NavScreen(),
+    );
+  }
+}
+
+class NavScreen extends StatelessWidget {
+  const NavScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
