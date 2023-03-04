4c530cb83e47ab9978c7d1c6800ac5feef659d36
vaishnavivarkur <91609019+vaishnavivarkur@users.noreply.github.com>
2023-03-04 15:27:08 +0530

    Update main.dart
    added 'apply to volunteer' card

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,120 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavScreen(),
+      //home: NavScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('card'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SizedBox(
+            height: 236,
+            width: 380,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(children: [
+                  Row(
+                    children: [
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        color: Colors.white70,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0,
+                            vertical: 6.0,
+                          ),
+                          child: Row(
+                            children: const [
+                              Icon(Icons.access_time),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text('starts on 4 March'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        color: Colors.white70,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0,
+                            vertical: 6.0,
+                          ),
+                          child: Row(
+                            children: const [
+                              Icon(Icons.location_on_outlined),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text('Pune'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(left: 8),
+                        height: 100,
+                        width: 100,
+                        color: Colors.yellowAccent,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 100,
+                          width: 270,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 10),
+                          child: const Text(
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown ",
+                            style: TextStyle(fontSize: 13),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.black87,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 100.0, vertical: 16),
+                      textStyle: const TextStyle(fontSize: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text('Apply to volunteer'),
+                  ),
+                ]),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
