6d359d0ef450670f3b4e5aa94d6fb2a85086c14f
tejas-xd <tskakde@gmail.com>
2023-03-25 14:09:15 +0530

    added function to create community
    Signed-off-by: tejas-xd <tskakde@gmail.com>

---
+++ lib/services/firestore.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> addcommunity(String contact,String description,String location,String logo,String mission,String website,String name)
+async {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  await _firestore
+      .collection('community')
+      .doc(name)
+      .set({
+    "contact" : contact,
+    "description" : description,
+    "location" : location,
+    "logo" : logo,
+    "mission" : mission,
+    "name" : name,
+    "website" : website,
+
+  });
+}
