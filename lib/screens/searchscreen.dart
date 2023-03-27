22cea16028831d33ceee16e5a799e832dbd91331
tejas-xd <tskakde@gmail.com>
2023-03-27 20:16:32 +0530

    added searchscreen
    Signed-off-by: tejas-xd <tskakde@gmail.com>

--- lib/screens/searchscreen.dart
+++ lib/screens/searchscreen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_size_text_field/auto_size_text_field.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hapii/screens/groupscreen.dart';
 import 'package:hapii/widgets/bottomNavBar.dart';
 
 import '../services/const.dart';
@@ -12,20 +14,24 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  String query = "";
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    TextEditingController logo = TextEditingController();
+    TextEditingController search = TextEditingController();
+    final CollectionReference CommunityCollection = FirebaseFirestore.instance.collection('community');
+
     return Scaffold(
+      resizeToAvoidBottomInset: false,
         body: Column(
           children: [
-            SizedBox(height: 60,),
+            const SizedBox(height: 60,),
             Container(
               alignment: Alignment.centerLeft,
               margin: const EdgeInsets.only(left: 20,right: 20,bottom: 10),
               width: size.width,
               height: 50,
-              child: AutoSizeTextField(
+              child: TextField(
                 decoration: InputDecoration(
                     suffixIcon: Icon(Icons.search),
                     focusedBorder: OutlineInputBorder(
@@ -43,13 +49,43 @@ class _SearchScreenState extends State<SearchScreen> {
                       borderSide: const BorderSide(color: Colors.black),
                       borderRadius: BorderRadius.circular(5),
                     )),
-                controller: logo,
+                controller: search,
                 style: const TextStyle(fontSize: 22),
-                minFontSize: 18,
+                onSubmitted: (value){
+                  setState(() {
+                    query = search.text;
+                  });
+                },
                 maxLines: 1,
 
               ),
             ),
+            Expanded(
+              child: FutureBuilder(
+                future: CommunityCollection.where('name', isGreaterThanOrEqualTo: query).get(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        padding: EdgeInsets.only(bottom: 10),
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          Map<String, dynamic> group =
+                          snapshot.data!.docs[index].data() as Map<String, dynamic>;
+                          return Group(
+                            size: size,
+                            Groupinfo: group['mission'],
+                            Grouplogo: group['logo'],
+                            Groupname: group['name'],
+                          );
+                        },
+                      );
+                    } else {
+                      return Container();
+                    }
+                  }
+              ),
+            )
           ],
         ));
   }
